54b036bbbd39ddfdd11e615fcdfa97e5a2c6aeff
heksen84 <heksen84@yandex.ru>
2018-03-10 22:27:10 +0600

    ok

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -26,19 +26,19 @@
                text-variant="white"
                header="Primary"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card bg-variant="secondary"
                text-variant="white"
                header="Secondary"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card bg-variant="success"
                text-variant="white"
                header="Success"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
    </b-card-group>
    <b-card-group deck class="mb-3">
@@ -46,19 +46,19 @@
                text-variant="white"
                header="Info"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card bg-variant="warning"
                text-variant="white"
                header="Warning"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card bg-variant="danger"
                text-variant="white"
                header="Danger"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
    </b-card-group>
    <b-card-group deck
@@ -66,23 +66,24 @@
        <b-card bg-variant="light"
                header="Light"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card bg-variant="dark"
                header="Dark"
                text-variant="white"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
        <b-card header="Default"
                class="text-center">
-           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           <p class="card-text">number info</p>
        </b-card>
-   </b-card-group>
+   	</b-card-group>
 
-	advert
+	advert	
     </b-col>
   </b-row>
+<!--<b-pagination :total-rows="100" :per-page="10"></b-pagination>-->
 </b-container>
 <!--<welcome></welcome>-->
 <!--@if (Route::has('login'))
