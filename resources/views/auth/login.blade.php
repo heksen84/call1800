aae45c55cbdb8d1c7fffb12dacaeaf576124ebcc
heksen84 <heksen84@yandex.ru>
2018-03-10 20:11:10 +0600

    ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="get numbers, free numbers" />
+        <meta name="description" content="get free numbers">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Call1800 - get free numbers.</title>
+        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+<div id="app"></div>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <script src="{{ asset('js/app.js') }}"></script>
 <center>
@@ -58,3 +73,4 @@
                         </div>
                     </form>
 </center>
+</body>
