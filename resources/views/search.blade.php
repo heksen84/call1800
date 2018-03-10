4568f9194fd0e2ff137e139f8d0ed4d882914aeb
heksen84 <heksen84@yandex.ru>
2018-03-10 23:39:51 +0600

    ok

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -20,57 +20,28 @@
         <h1 style="color:grey">Results</h1>
 	advert
       <br>
-      <b-card-group deck
-                 class="mb-3">
-       <b-card bg-variant="primary"
-               text-variant="white"
-               header="Primary"
+      <br>
+      <b-card-group deck class="mb-3">
+       <b-card header="Default"
                class="text-center">
            <p class="card-text">number info</p>
        </b-card>
-       <b-card bg-variant="secondary"
-               text-variant="white"
-               header="Secondary"
+
+       <b-card header="Default"
                class="text-center">
            <p class="card-text">number info</p>
        </b-card>
-       <b-card bg-variant="success"
-               text-variant="white"
-               header="Success"
+       <b-card header="Default"
                class="text-center">
            <p class="card-text">number info</p>
        </b-card>
-   </b-card-group>
-   <b-card-group deck class="mb-3">
-       <b-card bg-variant="info"
-               text-variant="white"
-               header="Info"
+   	</b-card-group>
+      <b-card-group deck class="mb-3">
+       <b-card header="Default"
                class="text-center">
            <p class="card-text">number info</p>
        </b-card>
-       <b-card bg-variant="warning"
-               text-variant="white"
-               header="Warning"
-               class="text-center">
-           <p class="card-text">number info</p>
-       </b-card>
-       <b-card bg-variant="danger"
-               text-variant="white"
-               header="Danger"
-               class="text-center">
-           <p class="card-text">number info</p>
-       </b-card>
-   </b-card-group>
-   <b-card-group deck
-                 class="mb-3">
-       <b-card bg-variant="light"
-               header="Light"
-               class="text-center">
-           <p class="card-text">number info</p>
-       </b-card>
-       <b-card bg-variant="dark"
-               header="Dark"
-               text-variant="white"
+       <b-card header="Default"
                class="text-center">
            <p class="card-text">number info</p>
        </b-card>
