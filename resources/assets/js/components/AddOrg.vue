2f79978a68047ea3989c2670fbe2fd133a80750b
heksen84 <heksen84@yandex.ru>
2018-03-18 18:52:41 +0600

    ok

--- resources/assets/js/components/AddOrg.vue
+++ resources/assets/js/components/AddOrg.vue
@@ -13,16 +13,31 @@
 </b-nav>
 <br>
 <b-form @submit="onSubmit" @reset="onReset">
+
+
+
       <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
+                    label="Number:"
                     label-for="exampleInput1"
                     description="We'll never share your email with anyone else.">
         <b-form-input id="exampleInput1"
-                      type="email"             
+                      type="text"             
                       required
-                      placeholder="Enter email">
+                      placeholder="Enter number">
         </b-form-input>
       </b-form-group>
+
+
+
+      <b-form-group id="exampleInputGroup1"
+                    label="Name:"
+                    label-for="exampleInput1"
+                    description="We'll never share your email with anyone else.">
+        <b-form-input id="exampleInput1"
+                      type="text"             
+                      required
+                      placeholder="Enter name">
+        </b-form-input>
       </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
