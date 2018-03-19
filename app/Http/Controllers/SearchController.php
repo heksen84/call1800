d636cf7c8ec0327db62a4d0f2293662c750b4cca
heksen84 <heksen84@yandex.ru>
2018-03-19 19:00:17 +0600

    ok

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,6 +8,45 @@ use App\Http\Controllers\Controller;
 use Vinelab\Http\Client as HttpClient;
 use App\Numbers;
 
+
+function jsonFixer($json){
+    $patterns     = [];
+    /** garbage removal */
+    $patterns[0]  = "/([\s:,\{}\[\]])\s*'([^:,\{}\[\]]*)'\s*([\s:,\{}\[\]])/"; //Find any character except colons, commas, curly and square brackets surrounded or not by spaces preceded and followed by spaces, colons, commas, curly or square brackets...
+    $patterns[1]  = '/([^\s:,\{}\[\]]*)\{([^\s:,\{}\[\]]*)/'; //Find any left curly brackets surrounded or not by one or more of any character except spaces, colons, commas, curly and square brackets...
+    $patterns[2]  =  "/([^\s:,\{}\[\]]+)}/"; //Find any right curly brackets preceded by one or more of any character except spaces, colons, commas, curly and square brackets...
+    $patterns[3]  = "/(}),\s*/"; //JSON.parse() doesn't allow trailing commas
+    /** reformatting */
+    $patterns[4]  = '/([^\s:,\{}\[\]]+\s*)*[^\s:,\{}\[\]]+/'; //Find or not one or more of any character except spaces, colons, commas, curly and square brackets followed by one or more of any character except spaces, colons, commas, curly and square brackets...
+    $patterns[5]  = '/["\']+([^"\':,\{}\[\]]*)["\']+/'; //Find one or more of quotation marks or/and apostrophes surrounding any character except colons, commas, curly and square brackets...
+    $patterns[6]  = '/(")([^\s:,\{}\[\]]+)(")(\s+([^\s:,\{}\[\]]+))/'; //Find or not one or more of any character except spaces, colons, commas, curly and square brackets surrounded by quotation marks followed by one or more spaces and  one or more of any character except spaces, colons, commas, curly and square brackets...
+    $patterns[7]  = "/(')([^\s:,\{}\[\]]+)(')(\s+([^\s:,\{}\[\]]+))/"; //Find or not one or more of any character except spaces, colons, commas, curly and square brackets surrounded by apostrophes followed by one or more spaces and  one or more of any character except spaces, colons, commas, curly and square brackets...
+    $patterns[8]  = '/(})(")/'; //Find any right curly brackets followed by quotation marks...
+    $patterns[9]  = '/,\s+(})/'; //Find any comma followed by one or more spaces and a right curly bracket...
+    $patterns[10] = '/\s+/'; //Find one or more spaces...
+    $patterns[11] = '/^\s+/'; //Find one or more spaces at start of string...
+
+    $replacements     = [];
+    /** garbage removal */
+    $replacements[0]  = '$1 "$2" $3'; //...and put quotation marks surrounded by spaces between them;
+    $replacements[1]  = '$1 { $2'; //...and put spaces between them;
+    $replacements[2]  = '$1 }'; //...and put a space between them;
+    $replacements[3]  = '$1'; //...so, remove trailing commas of any right curly brackets;
+    /** reformatting */
+    $replacements[4]  = '"$0"'; //...and put quotation marks surrounding them;
+    $replacements[5]  = '"$1"'; //...and replace by single quotation marks;
+    $replacements[6]  = '\\$1$2\\$3$4'; //...and add back slashes to its quotation marks;
+    $replacements[7]  = '\\$1$2\\$3$4'; //...and add back slashes to its apostrophes;
+    $replacements[8]  = '$1, $2'; //...and put a comma followed by a space character between them;
+    $replacements[9]  = ' $1'; //...and replace by a space followed by a right curly bracket;
+    $replacements[10] = ' '; //...and replace by one space;
+    $replacements[11] = ''; //...and remove it.
+
+    $result = preg_replace($patterns, $replacements, $json);
+
+    return $result;
+  }
+
 class SearchController extends Controller
 {
     public function Search(Request $request) {
@@ -19,11 +58,9 @@ class SearchController extends Controller
 
         $attachment_ids = array();
 
-	       $url = array(
-			        "http://151.80.37.10:5000/tollfreenumber?query=",
-			           "http://151.80.37.10:5000/inter800?query=",
-			              "http://151.80.37.10:5000/tollfreeda?query="
-			               );
+	       $url = array( "http://151.80.37.10:5000/tollfreenumber?query=",
+			                 "http://151.80.37.10:5000/inter800?query=",
+			                 "http://151.80.37.10:5000/tollfreeda?query=");
 
       // 0 server
        if ($request->server_index==0){
