5edd35b9d3e77b82ffc2f89e64c74fb68730f194
Илья <heksen84@yandex.ru>
2018-03-23 17:31:38 +0600

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -51782,7 +51782,7 @@ var render = function() {
                           {
                             staticStyle: {
                               color: "rgb(140,140,140)",
-                              "margin-top": "100px"
+                              "margin-top": "50px"
                             }
                           },
                           [_vm._v("please wait")]
