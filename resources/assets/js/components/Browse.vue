af83ef8e78504f4c3ba8b90c93c8579717b2a62e
heksen84 <heksen84@yandex.ru>
2018-03-28 21:05:53 +0600

    ok

--- resources/assets/js/components/Browse.vue
+++ resources/assets/js/components/Browse.vue
@@ -149,6 +149,34 @@
     },
        created() {
 
+
+
+         /*function httpGet(theUrl)
+         {
+             if (window.XMLHttpRequest)
+             {// code for IE7+, Firefox, Chrome, Opera, Safari
+                 xmlhttp=new XMLHttpRequest();
+             }
+             else
+             {// code for IE6, IE5
+                 xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+             }
+             xmlhttp.onreadystatechange=function()
+             {
+                 if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                 {
+                     return xmlhttp.responseText;
+                 }
+             }
+             xmlhttp.open("GET", theUrl, false );
+             xmlhttp.send();
+         }*/
+
+
+
+
+
+
        this.items1 = {}
        this.items2 = {}
        this.items3 = {}
